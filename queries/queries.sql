0daf8ab570b46f04e025c63c845041e909bff13c
Riu <josemanuelriu@gmail.com>
2026-01-20 13:24:34 +0100

    Add SQL queries for degrees and student data
    Added SQL queries to retrieve data about degrees, credits, and the youngest student.

--- queries/queries.sql
+++ queries/queries.sql
@@ -129,10 +129,16 @@ GROUP BY g.nombre
 ORDER BY total DESC;
 
 -- 21. Retorna un llistat amb el nom de tots els graus existents en la base de dades i el nombre d'assignatures que té cadascun, dels graus que tinguin més de 40 assignatures associades. (grau, total)
-
+SELECT g.nombre as grau, COUNT(a.nombre) AS total
+FROM grado g
+LEFT JOIN asignatura a ON a.id_grado=g.id
+WHERE total >40;
 
 -- 22. Retorna un llistat que mostri el nom dels graus i la suma del nombre total de crèdits que hi ha per a cada tipus d'assignatura. El resultat ha de tenir tres columnes: nom del grau, tipus d'assignatura i la suma dels crèdits de totes les assignatures que hi ha d'aquest tipus. (grau, tipus, total_creditos)
-
+SELECT g.nombre as grau, a.tipo as tipus, COUNT(a.nombre) as total_creditos
+FROM grado g
+INNER JOIN asignatura a ON a.id_grado=g.id
+GROUP BY a.nombre,  g.nombre;
 
 -- 23. Retorna un llistat que mostri quants alumnes s'han matriculat d'alguna assignatura en cadascun dels cursos escolars. El resultat haurà de mostrar dues columnes, una columna amb l'any d'inici del curs escolar i una altra amb el nombre d'alumnes matriculats. (anyo_inicio, total)
 
@@ -141,7 +147,10 @@ ORDER BY total DESC;
 
 
 -- 25. Retorna totes les dades de l'alumne/a més jove. (*)
-
+SELECT * FROM persona
+WHERE tipo="alumno"
+ORDER BY fecha_nacimiento DESC
+LIMIT 1; 
 
 -- 26. Retorna un llistat amb els professors/es que tenen un departament associat i que no imparteixen cap assignatura. (apellido1, apellido2, nombre)
 
